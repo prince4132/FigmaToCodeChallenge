82e46752984f065a81b2f42b14b195fdb25ee3a5
Prince <princeekpinse97@gmail.com>
2024-08-01 19:24:20 +0100

    Navbar

--- app/components/CTA.jsx
+++ app/components/CTA.jsx
@@ -4,10 +4,12 @@ function CTA() {
   return (
     <div>
       <div   className='flex flex-col rounded-xl border min-h-[400px] CTA mt-[30px] w-full gap-[30px] items-center justify-center'>
-            <h1 className='text-4xl text-neutral-50'>Build your NFT profile</h1>
-            <h2 className="text-2xl text-neutral-50">Join almost 10k NFT profile on Digit !</h2>
+            <h1 className='text-4xl text-white'>Build your NFT profile</h1>
+
+            <h2 className="text-2xl text-white">Join almost 10k NFT profile on Digit !</h2>
+
             <a href="#">
-                <button className='bg-neutral-50 p-5 px-10 rounded-lg  text-neutral-950'>
+                <button className='bg-neutral-50 hover:bg-neutral-200 p-5 px-10 rounded-3xl  text-neutral-950'>
                     Sign in now
                 </button>
             </a>
